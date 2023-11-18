4026b2ae744a319bc161426a9a62f4393efd78ce
Danil <danil.r.savchenko@gmail.com>
2023-11-18 14:29:26 +0200

    fix task model

--- TaskManager/tasks/models.py
+++ TaskManager/tasks/models.py
@@ -11,11 +11,11 @@ class Category(models.Model):
 class Task(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    description = models.TextField
-    deadline = models.DateTimeField
-    priority = models.IntegerField
-    status = models.CharField
-    creation_date = models.DateTimeField
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    priority = models.IntegerField()
+    status = models.CharField(max_length=100)
+    creation_date = models.DateTimeField()
 
     def __str__(self):
         return f"Task: {self.title},  status: {self.status},  deadline: {self.deadline}"
